a1c5731c96acea5ab843c1ee6d797d4ec2030d15
rdcassin <rdcassin@gmail.com>
2025-05-25 17:18:27 -0500

    restructure to have code in repl.go and have main just run repl()

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"strings"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		text := cleanInput(input)
-		if len(text) > 0 {
-		fmt.Printf("Your command was: %s\n", text[0])
-		} else {
-			fmt.Println("No command entered.")
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	lowerCaseText:= strings.ToLower(text)
-	splitText := strings.Fields(lowerCaseText)
-	return splitText
+	repl()
 }
