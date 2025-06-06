e072dea88c1691bcdec852effa6200c305af50e5
rdcassin <rdcassin@gmail.com>
2025-06-06 13:42:25 -0500

    Added additional Test Reap Loop test

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -59,3 +59,32 @@ func TestReapLoop(t *testing.T) {
 		return
 	}
 }
+
+func TestReapLoop2(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 8 * time.Millisecond
+	cache := NewCache(baseTime)
+	cache.Add("https://example.com", []byte("testdata"))
+
+	_, ok := cache.Get("https://example.com")
+	if !ok {
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get("https://example.com")
+	if ok {
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get("https://example.com")
+	if ok {
+		t.Errorf("expected not to find key")
+		return
+	}
+}
